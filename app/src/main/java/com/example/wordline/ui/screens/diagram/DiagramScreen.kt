fadbe68d33b2546332555ac45ab71c19796afa03
Falke Warming <Falkewj@gmail.com>
2025-02-22 15:50:30 +0100

    Back on UI made card of linesettings draggable. To do: Contraints on drag. shape of card. snap to small bar when close. Fling to close functionality. Consider: Create setting for snap to size or variable size

--- app/src/main/java/com/example/wordline/ui/screens/diagram/DiagramScreen.kt
+++ app/src/main/java/com/example/wordline/ui/screens/diagram/DiagramScreen.kt
@@ -2,18 +2,30 @@ package com.example.wordline.ui.screens.diagram
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -21,7 +33,9 @@ import androidx.compose.ui.graphics.vector.VectorProperty
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -30,36 +44,61 @@ import com.example.wordline.data.model.Line
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DiagramScreen(modifier: Modifier = Modifier, viewModel: DiagramViewModel = viewModel()) {
-    //val testInstruction = viewModel.getTestInstruction()
-    Column(modifier = Modifier.fillMaxSize()) {
-        Canvas(modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Blue)
-            .onSizeChanged { size ->
-                viewModel.updateCanvasSize(size.toSize())
-            }
-            .pointerInput(Unit) {
-                detectTapGestures { offset ->
-                    viewModel.onLineTapped(offset)
+    val density = LocalDensity.current
+    var boxHeight by remember { mutableStateOf(0.dp) }
+    val minCardHeight = 40.dp
+    val canvasHeight = boxHeight - minCardHeight
+    var cardHeight by remember { mutableStateOf(minCardHeight) }
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .onSizeChanged { size ->
+            boxHeight = with(density) { size.height.toDp() }
+        }
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(canvasHeight)
+                .background(Color.Blue)
+                .onSizeChanged { size ->
+                    viewModel.updateCanvasSize(size.toSize())
                 }
-            }
+                .pointerInput(Unit) {
+                    detectTapGestures { offset ->
+                        viewModel.onLineTapped(offset)
+                    }
+                }
         ) {
             drawLine(
-//                start = testInstruction.start,
-//                end = testInstruction.end,
-//                color = testInstruction.color,
-//                strokeWidth = testInstruction.width
+//                  start = testInstruction.start,
+//                  end = testInstruction.end,
+//                  color = testInstruction.color,
+//                  strokeWidth = testInstruction.width
                 start = Offset(x = size.width, y = 0f),
                 end = Offset(x = 0f, y = size.height),
                 color = Color.Red,
                 strokeWidth = 5f
             )
         }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(cardHeight)
+                .draggable(
+                    orientation = Orientation.Vertical,
+                    state = rememberDraggableState { delta ->
+                        cardHeight -= with(density) { delta.toDp() }
+                    }
+                )
+                .align(Alignment.BottomCenter)
+        ) {
+            LineSettings()
+        }
     }
 }
 
 @Composable
-fun LineInfo(modifier: Modifier = Modifier) {
+fun LineSettings(modifier: Modifier = Modifier) {
     Text(modifier = modifier, text = "Placeholder")
 }
 
@@ -68,8 +107,10 @@ fun LineInfo(modifier: Modifier = Modifier) {
 @Composable
 private fun DiagramScreenPreview() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        DiagramScreen(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding))
+        DiagramScreen(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        )
     }
 }
