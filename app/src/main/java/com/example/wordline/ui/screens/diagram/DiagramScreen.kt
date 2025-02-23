9701432144d68cf985be609bf1375587b0434b42
Falke Warming <Falkewj@gmail.com>
2025-02-23 16:46:29 +0100

    formatted card corners and elevation. Changed canvas to take up the full screen because i don't even remember why it didn't.

--- app/src/main/java/com/example/wordline/ui/screens/diagram/DiagramScreen.kt
+++ app/src/main/java/com/example/wordline/ui/screens/diagram/DiagramScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,7 +39,6 @@ fun DiagramScreen(modifier: Modifier = Modifier, viewModel: DiagramViewModel = v
     val density = LocalDensity.current
     var boxHeight by remember { mutableStateOf(0.dp) }
     val minCardHeight = 40.dp
-    val canvasHeight = boxHeight - minCardHeight
     var cardHeight by remember { mutableStateOf(minCardHeight) }
     val velocityTracker = remember { VelocityTracker() }
     var prevCardHeight = remember { minCardHeight }
@@ -52,8 +53,7 @@ fun DiagramScreen(modifier: Modifier = Modifier, viewModel: DiagramViewModel = v
     ) {
         Canvas(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(canvasHeight)
+                .fillMaxSize()
                 .background(Color.Blue)
                 .onSizeChanged { size ->
                     viewModel.updateCanvasSize(size.toSize())
@@ -72,6 +72,8 @@ fun DiagramScreen(modifier: Modifier = Modifier, viewModel: DiagramViewModel = v
             )
         }
         Card(
+            elevation = CardDefaults.cardElevation(0.dp),
+            shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .height(cardHeight)
